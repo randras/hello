0fb8249050288696832b843952e4c2630c48f8a3
randras <andras.romhanyi@appello.eu>
2019-03-18 15:48:13 +0100

    profile picture

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import profilePicture from "../logo.svg";
+import profilePicture from "../me.png";
 import { Dictionary } from "../content/Dictionary";
 
 
@@ -12,7 +12,7 @@ export default class AppHeader extends React.Component {
                     <div className="jumbotron">
                         <div className="container">
                             <div className="heading">
-                                <img className="img" src={profilePicture} alt="Profile picture"/>
+                                <img className="img rounded-circle" src={profilePicture} alt="Profile picture"/>
                             </div>
                             <div className="heading">
                                 <h1 className="display-4">{Dictionary.get("hello")}</h1>
